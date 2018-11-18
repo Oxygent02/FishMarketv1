bfd24af38bd8f2b987dfa6e1267c4f788bd3a30e
Reinhard JS <reinhardjsilalahi@gmail.com>
2018-11-18 11:25:33 +0700

    commit

--- app/src/main/java/google/projectbuilding/fishmarket_v1/MainActivity.java
+++ app/src/main/java/google/projectbuilding/fishmarket_v1/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
     // Perubahan master
 
+    // Perubahan master 2
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
