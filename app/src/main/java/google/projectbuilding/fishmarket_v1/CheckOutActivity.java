5246af4d53e7e5cbe158453efb41ab82495c51fc
Oxygent02 <fy_danny@yahoo.com>
2018-11-18 09:19:46 +0700

    Salah variable alamat. Sudah bener

--- app/src/main/java/google/projectbuilding/fishmarket_v1/CheckOutActivity.java
+++ app/src/main/java/google/projectbuilding/fishmarket_v1/CheckOutActivity.java
@@ -2,6 +2,7 @@ package google.projectbuilding.fishmarket_v1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CheckOutActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class CheckOutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_out);
+    }
+
+    public void click_minus(View view) {
+    }
+
+    public void click_tambah(View view) {
+    }
+
+    public void click_bayar(View view) {
     }
 }
